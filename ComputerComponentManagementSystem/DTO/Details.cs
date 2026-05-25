13bbba5896456ff33a2ded9d8f434d289f7ad808
TonyAnthony1 <tonyanthonyyyyy@gmail.com>
2026-05-25 12:49:06 +0200

    Added DTO baranch and dto classes

---
+++ ComputerComponentManagementSystem/DTO/Details.cs
@@ -0,0 +1,42 @@
+﻿namespace ComputerComponentManagementSystem.DTO;
+
+public class Details
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public double Weight { get; set; }
+    public int Warranty { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public int Stock { get; set; }
+    public List<PCComponentDto> Components { get; set; } = new();
+}
+
+public class PCComponentDto
+{
+    public int Amount { get; set; }
+    public ComponentDto Component { get; set; } = null!;
+}
+
+public class ComponentDto
+{
+    public string Code { get; set; } = null!;
+    public string Name { get; set; } = null!;
+    public string? Description { get; set; }
+    public ManufacturerDto Manufacturer { get; set; } = null!;
+    public ComponentTypeDto Type { get; set; } = null!;
+}
+
+public class ManufacturerDto
+{
+    public int Id { get; set; }
+    public string Abbreviation { get; set; } = null!;
+    public string FullName { get; set; } = null!;
+    public DateTime FoundationDate { get; set; }
+}
+
+public class ComponentTypeDto
+{
+    public int Id { get; set; }
+    public string Abbreviation { get; set; } = null!;
+    public string Name { get; set; } = null!;
+}
---
+++ ComputerComponentManagementSystem/DTO/ListItem.cs
@@ -0,0 +1,11 @@
+﻿namespace ComputerComponentManagementSystem.DTO;
+
+public class ListItem
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public double Weight { get; set; }
+    public int Warranty { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public int Stock { get; set; }
+}
