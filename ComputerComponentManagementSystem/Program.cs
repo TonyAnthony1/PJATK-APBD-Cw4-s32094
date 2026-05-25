35bf43a37b5ee470e3a0d290de095e7ead1262bd
TonyAnthony1 <tonyanthonyyyyy@gmail.com>
2026-05-25 14:09:36 +0200

    change in program.cs

--- ComputerComponentManagementSystem/Program.cs
+++ ComputerComponentManagementSystem/Program.cs
@@ -1,23 +1,27 @@
+using ComputerComponentManagementSystem.Data;
+using ComputerComponentManagementSystem.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IService, Service>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
